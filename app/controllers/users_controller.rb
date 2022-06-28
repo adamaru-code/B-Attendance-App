372698b03e1420e03596acbee0a9a67a9257b16a
adamaru <adamaru8700@icloud.com>
2022-06-28 01:18:18 +0000

    検索フォーム 追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
   def index
     # @users = User.all
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page]).search(params[:search])
   end
   
   def show
